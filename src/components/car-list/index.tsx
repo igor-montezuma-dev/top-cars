06be60f2a210dc1ce79c5a10c3315b201a30acae
IgorMontezuma20 <igorminranda17@gmail.com>
2024-06-30 17:07:22 -0300

    feat: Fetch cars data from api

--- src/components/car-list/index.tsx
+++ src/components/car-list/index.tsx
@@ -1,5 +1,5 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import data from "../../data/cars.json";
 
 interface Vehicle {
   id: number;
@@ -11,7 +11,19 @@ interface Vehicle {
   cor: string;
   nome_modelo: string;
   valor: number;
-  brand: number;
+  model: Model;
+}
+
+interface Model {
+  id: number;
+  nome: string;
+  valor_fipe: number;
+  brand: Brand;
+}
+
+interface Brand {
+  marca_id: number;
+  nome_marca: string;
 }
 
 const VehicleList = () => {
@@ -19,28 +31,35 @@ const VehicleList = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      setVehicles(data.cars);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Erro ao processar veículos:", error);
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://top-cars-api.onrender.com/cars"
+        );
+        setVehicles(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Erro ao processar veículos:", error);
+      }
     }
+    fetchData();
   }, []);
 
   type GroupedVehicles = {
     [key: string]: Vehicle[];
   };
 
-  const groupedVehicles: GroupedVehicles = Array.isArray(vehicles)
-    ? vehicles.reduce((acc: GroupedVehicles, vehicle: Vehicle) => {
-        const { brand } = vehicle;
-        if (!acc[brand]) {
-          acc[brand] = [];
-        }
-        acc[brand].push(vehicle);
-        return acc;
-      }, {})
-    : {};
+  const groupedVehicles: GroupedVehicles = vehicles.reduce(
+    (acc: GroupedVehicles, vehicle: Vehicle) => {
+      const brandName = vehicle.model.brand.nome_marca;
+      if (!acc[brandName]) {
+        acc[brandName] = [];
+      }
+      acc[brandName].push(vehicle);
+      return acc;
+    },
+    {}
+  );
 
   const sortedBrands = Object.keys(groupedVehicles).sort();
 
@@ -52,11 +71,11 @@ const VehicleList = () => {
 
   return (
     <div className="container mx-auto p-4">
-      {sortedBrands.map((brand) => (
-        <div key={brand} className="mb-6">
-          <h2 className="text-2xl font-bold mb-4">Marca: {brand}</h2>
+      {sortedBrands.map((brandName) => (
+        <div key={brandName} className="mb-6">
+          <h2 className="text-2xl font-bold mb-4">Marca: {brandName}</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {groupedVehicles[brand].map((vehicle) => (
+            {groupedVehicles[brandName].map((vehicle) => (
               <div
                 key={vehicle.id}
                 className="p-4 border bg-gray-50 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
